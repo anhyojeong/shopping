78f31ebff8fd7adb4bc4b0e7d4004ead3f29256d
HyoJeong <hyozeong1230@gmail.com>
2023-11-10 04:50:56 +0900

    화면 구현 : 로그인

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-function App() {
+import React, { useState } from 'react';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+import "./css/sign.css";
+
+const App = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const togglePanel = () => {
+    setIsSignUp(!isSignUp);
+  };
+
   return (
-    <div className="App">
+    <div className={`container ${isSignUp ? 'right-panel-active' : ''}`}>
+      <SignIn>
+      </SignIn>
+      <SignUp></SignUp>
+      <div className="overlay-container">
+        <div className="overlay">
+          <div className="overlay-panel overlay-left">
+            <h1 className="sign-title">어서오세요!</h1>
+            <p className = 'sign-subTitle'>계정이 있으시다면 로그인 후 이용해 주세요.</p>
+            <button className="hidden" onClick={togglePanel} id="signIn">
+              로그인
+            </button>
+          </div>
+          <div className="overlay-panel overlay-right">
+            <h1 className="sign-title">반가워요!</h1>
+            <p className = 'sign-subTitle' >계정이 없다면 회원가입을 먼저 해주세요.</p>
+            <button className="hidden" onClick={togglePanel} id="signUp">
+              회원가입
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
